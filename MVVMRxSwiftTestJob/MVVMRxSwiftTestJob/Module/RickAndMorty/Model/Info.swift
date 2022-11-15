6d2a588082318b2c79b8eedf6c373e38fae69178
swiftproger <swiftapp@mail.ru>
2022-11-15 21:48:03 +0300

    edit model info

--- MVVMRxSwiftTestJob/MVVMRxSwiftTestJob/Module/RickAndMorty/Model/Info.swift
+++ MVVMRxSwiftTestJob/MVVMRxSwiftTestJob/Module/RickAndMorty/Model/Info.swift
@@ -8,5 +8,33 @@
 import Foundation
 
 struct Info: Codable {
+    private var count: Int?
+    private var pages: Int?
+    private var next: String?
     
+    enum CodingKeys: CodingKey {
+        case count, pages, next
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.count = try container.decodeIfPresent(Int.self, forKey: .count)
+        self.pages = try container.decodeIfPresent(Int.self, forKey: .pages)
+        self.next = try container.decodeIfPresent(String.self, forKey: .next)
+    }
+    
+    public func getCount() -> Int {
+        guard let count = count else { return 0 }
+        return count
+    }
+    
+    public func getPages() -> Int {
+        guard let pages = pages else { return 0 }
+        return pages
+    }
+    
+    public func getNextLink() -> String {
+        guard let next = next else { return "" }
+        return next
+    }
 }
