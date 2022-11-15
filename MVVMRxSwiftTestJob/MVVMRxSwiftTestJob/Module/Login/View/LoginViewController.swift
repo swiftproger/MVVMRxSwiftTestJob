28ffe37bbee53b53511028bbd82048ba6f92ae20
swiftproger <swiftapp@mail.ru>
2022-11-15 21:05:01 +0300

    ui LoginViewController

--- MVVMRxSwiftTestJob/MVVMRxSwiftTestJob/Module/Login/View/LoginViewController.swift
+++ MVVMRxSwiftTestJob/MVVMRxSwiftTestJob/Module/Login/View/LoginViewController.swift
@@ -8,9 +8,63 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    lazy var loginTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "login"
+        textField.borderStyle = .roundedRect
+        textField.keyboardType = .emailAddress
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "password"
+        textField.borderStyle = .roundedRect
+        textField.isSecureTextEntry = true
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    lazy var loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.setTitleColor(.white.withAlphaComponent(0.3), for: .highlighted)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 6
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(loginButtonAction), for: .touchUpInside)
+        return button
+    }()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello LoginViewController")
+        setupView()
+    }
+    
+    func setupView() {
+        self.view.backgroundColor = .systemBackground
+        self.view.addSubview(loginTextField)
+        self.view.addSubview(passwordTextField)
+        self.view.addSubview(loginButton)
+        
+        NSLayoutConstraint.activate([
+            loginTextField.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            loginTextField.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            loginTextField.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            passwordTextField.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            passwordTextField.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            passwordTextField.topAnchor.constraint(equalTo: self.loginTextField.bottomAnchor, constant: 20),
+            loginButton.topAnchor.constraint(equalTo: self.passwordTextField.bottomAnchor, constant: 20),
+            loginButton.widthAnchor.constraint(equalTo: passwordTextField.widthAnchor),
+            loginButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+        ])
+    }
+    
+    @objc
+    func loginButtonAction() {
+        print("i am button")
     }
 }
