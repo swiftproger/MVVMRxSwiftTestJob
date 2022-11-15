81812f7e8a9b624fca6c9ce7f2ba0711140724bd
swiftproger <swiftapp@mail.ru>
2022-11-15 13:20:18 +0300

    init project

---
+++ MVVMRxSwiftTestJob/MVVMRxSwiftTestJob/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMRxSwiftTestJob
+//
+//  Created by Александр Новиков on 15.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
