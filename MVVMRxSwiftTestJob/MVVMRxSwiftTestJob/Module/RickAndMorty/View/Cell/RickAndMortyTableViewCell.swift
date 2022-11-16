1fa1c8d43ceec2d6d7ca78f28c9236cf5410b8da
swiftproger <swiftapp@mail.ru>
2022-11-16 22:06:10 +0300

    create RickAndMortyTableViewCell

---
+++ MVVMRxSwiftTestJob/MVVMRxSwiftTestJob/Module/RickAndMorty/View/Cell/RickAndMortyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RickAndMortyTableViewCell.swift
+//  MVVMRxSwiftTestJob
+//
+//  Created by Александр Новиков on 16.11.2022.
+//
+
+import UIKit
+
+class RickAndMortyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
