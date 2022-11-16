db20737a0f7af1f0f1a3659a4fdb8fe4107b509f
swiftproger <swiftapp@mail.ru>
2022-11-16 22:15:27 +0300

    ui RickAndMortyViewController

--- MVVMRxSwiftTestJob/MVVMRxSwiftTestJob/Module/RickAndMorty/View/RickAndMortyViewController.swift
+++ MVVMRxSwiftTestJob/MVVMRxSwiftTestJob/Module/RickAndMorty/View/RickAndMortyViewController.swift
@@ -6,11 +6,37 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class RickAndMortyViewController: UIViewController {
-
+final class RickAndMortyViewController: UIViewController {
+    
+    private var viewModel = RickAndMortyViewModel()
+    private var bag = DisposeBag()
+    
+    private lazy var tableView: UITableView = {
+        var tableView = UITableView(frame: self.view.frame, style: .insetGrouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(RickAndMortyTableViewCell.self, forCellReuseIdentifier: "RickAndMortyTableViewCell")
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello RickAndMortyViewController")
+        setupUI()
+        getData()
+        bindTibleView()
+    }
+    
+    private func setupUI() {
+        self.view.addSubview(tableView)
+    }
+    
+    private func getData() {
+        
+    }
+    
+    private func bindTibleView() {
+        
     }
 }
